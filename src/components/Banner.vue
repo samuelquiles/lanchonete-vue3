f5cf20d25d8d8b3b675b9db80b1bf61f4eac70d7
Samuel de Oliveira <samuel.quiles@zaffari.com.br>
2023-03-08 17:26:34 -0300

    meu burguer

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+
+    #main-banner {
+        width: auto;
+        background-image: url('~@/assets/burger.jpg');
+        background-position: 0 -470px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        
+        padding: 20px 40px;
+    }
+</style>
